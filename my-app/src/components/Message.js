1d108a6ee3a5bf121096cea0f9aa170c3a2d00ee
Dima <benensondimitrii@gmail.com>
2022-03-08 16:52:22 +0200

    Message component style

--- my-app/src/components/Message.js
+++ my-app/src/components/Message.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import style from "./Message.module.css";
 
 const Message = () => {
   return (
-    <div>Message</div>
-  )
-}
+    <div className={style.Message}>
+      <div className={style.UserIcon}>R</div>
+      <div className={style.MessageBody}>
+        <div className={style.MessageDate}>sunday 13:13AM</div>
+        <div className={style.MessageText}>blabalbal</div>
+      </div>
+    </div>
+  );
+};
 
-export default Message
+export default Message;
