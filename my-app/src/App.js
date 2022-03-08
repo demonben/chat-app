a56869fd5873753e42f6cf462b80b492411e94c3
Dima <benensondimitrii@gmail.com>
2022-03-08 13:29:09 +0200

    added mock users and mock messages data and deleted default styles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,33 @@
-import "./App.css";
+const users = [
+  {
+    id: 1,
+    name: "Dima",
+  },
+  { id: 2,
+    name: "Lisa" },
+];
+
+const message = [
+  {
+    id: 1,
+    body: "Hello Lisa",
+    createdDate: Date.now(),
+    sender: "Dima",
+    senderId:1
+  },
+  {
+    id: 2,
+    body: "Hello Dima",
+    createdDate: Date.now(),
+    sender: "Lisa",
+    senderId:2
+  },
+];
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    
+  </div>;
 }
 
 export default App;
