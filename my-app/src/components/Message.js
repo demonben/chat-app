5fd952a744a48213676b5305d57a3fa7e385f0e7
Dima <benensondimitrii@gmail.com>
2022-03-08 17:24:35 +0200

    finished message style

--- my-app/src/components/Message.js
+++ my-app/src/components/Message.js
@@ -1,14 +1,18 @@
 import React from "react";
 import style from "./Message.module.css";
-import format from 'date-fns/format'
+import format from "date-fns/format";
+import { users } from "../mockDate";
+
 const Message = (props) => {
   const { message } = props;
-  // const sender = 
+  const sender = users.find((user) => user.id === message.senderId);
   return (
     <div className={style.Message}>
-      <div className={style.UserIcon}>R</div>
+      <div className={style.UserIcon}>{sender.name.substring(0, 1)}</div>
       <div className={style.MessageBody}>
-        <div className={style.MessageDate}>{format(message.createdDate, 'EEEE p')}</div>
+        <div className={style.MessageDate}>
+          {format(message.createdDate, "EEEE p")}
+        </div>
         <div className={style.MessageText}>{message.body}</div>
       </div>
     </div>
