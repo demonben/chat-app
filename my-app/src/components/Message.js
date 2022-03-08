aa718a22f87ef11aed72029db9d0cd65b9b99b6f
Dima <benensondimitrii@gmail.com>
2022-03-08 17:03:04 +0200

    added formater for date and body for messages

--- my-app/src/components/Message.js
+++ my-app/src/components/Message.js
@@ -1,13 +1,15 @@
 import React from "react";
 import style from "./Message.module.css";
-
-const Message = () => {
+import format from 'date-fns/format'
+const Message = (props) => {
+  const { message } = props;
+  // const sender = 
   return (
     <div className={style.Message}>
       <div className={style.UserIcon}>R</div>
       <div className={style.MessageBody}>
-        <div className={style.MessageDate}>sunday 13:13AM</div>
-        <div className={style.MessageText}>blabalbal</div>
+        <div className={style.MessageDate}>{format(message.createdDate, 'EEEE p')}</div>
+        <div className={style.MessageText}>{message.body}</div>
       </div>
     </div>
   );
