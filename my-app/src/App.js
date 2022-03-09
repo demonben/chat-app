aee54e289d4d6295fe9bbc77cb448bbc9adbccbb
Dima <benensondimitrii@gmail.com>
2022-03-09 17:43:17 +0200

    adding firebase config

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import MessagesList from "./components/MessagesList";
 import Navbar from "./components/Navbar";
 import NewMessageForm from "./components/NewMessageForm";
 import styles from "./App.module.css";
 import { messages as MockMessage } from "./mockDate";
-import { useState } from "react";
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBLATOMAS_t-TLsuwsRwZJzxZe5rStXsGU",
+  authDomain: "dima-ben-project.firebaseapp.com",
+  projectId: "dima-ben-project",
+  storageBucket: "dima-ben-project.appspot.com",
+  messagingSenderId: "17483853397",
+  appId: "1:17483853397:web:3b21cecdff4949275e6edc"
+};
+
+firebase.initializeApp(firebaseConfig)
 
 function App() {
   const [messages, setMessages] = useState(MockMessage);
