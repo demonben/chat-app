f379e10567fe5ad063fcfabde86419dffa42cbe6
Dima <benensondimitrii@gmail.com>
2022-03-10 15:53:01 +0200

    pop up auth google

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,10 +1,10 @@
 import React, { useLayoutEffect } from "react";
 import firebase from "firebase/compat/app";
-import * as firebaseui from "firebaseui"; 
-import 'firebaseui/dist/firebaseui.css'
+import * as firebaseui from "firebaseui";
+import "firebaseui/dist/firebaseui.css";
 
 const Login = () => {
-    // useLayoutEffect waits for the div to be in the DOM
+  // useLayoutEffect waits for the div to be in the DOM
   useLayoutEffect(() => {
     const ui =
       firebaseui.auth.AuthUI.getInstance() ||
@@ -14,7 +14,7 @@ const Login = () => {
         // List of OAuth providers supported.
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       ],
-      // Other config options...
+      signInFlow: "popup",
     });
   }, []);
 
