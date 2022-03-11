a2ecadc42e5e4567c6811a58b395fa935b7a729c
Dima <benensondimitrii@gmail.com>
2022-03-11 15:47:14 +0200

    replaced mock data from firestore

--- my-app/src/components/Message.js
+++ my-app/src/components/Message.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./Message.module.css";
 import format from "date-fns/format";
-import { users } from "../mockDate";
+import firebase from "firebase/compat/app";
+import { logDOM } from "@testing-library/react";
 
 const Message = (props) => {
+  const [sender, setSender] = useState(null);
   const { message } = props;
-  // const sender = users.find((user) => user.id === message.senderId);
+  const { senderId } = message;
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("users")
+      .doc(senderId)
+      .get()
+      .then((doc) => {
+        const sender = {
+          ...doc.data(),
+          id: doc.id,
+        };
+        setSender(sender);
+      });
+  }, [senderId]);
   return (
     <div className={style.Message}>
-      {/* <div className={style.UserIcon}>{sender.name.substring(0, 1)}</div> */}
+      <div className={style.UserIcon}>
+        {sender ? sender.name.substring(0, 1) : ""}
+      </div>
       <div className={style.MessageBody}>
         <div className={style.MessageDate}>
           {format(message.createdDate, "EEEE p")}
